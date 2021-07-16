22d4efa23326142ce150eb9d9b48cc46bff83bbd
Sean Ho <seanho96@yahoo.com>
2021-07-16 22:45:48 +0800

    add type checks

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,37 @@
-import { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import lottie from 'lottie-web'
 import Layout from '../components/Layout'
 
 const IndexPage = () => {
-  const [items, setItems] = useState<IItems[]>([])
+  const [items, setItems] = useState<string[]>([])
   const [random, setRandom] = useState<string>('')
   const [text, setText] = useState<string>('')
 
-  interface IItems {
-    items: string[]
-  }
-
-
   const borderColors = ['border-red-300', 'border-green-300', 'border-blue-300', 'border-yellow-300', 'border-orange-300', 'border-purple-300', 'border-pink-300', 'border-indigo-300']
-  const makeRepeated = (arr, repeats) => Array.from({ length: repeats }, () => arr).flat();
+  const makeRepeated = (arr: string[], repeats: number) => Array.from({ length: repeats }, () => arr).flat();
   const borderColorsArray = makeRepeated(borderColors, 10)
 
   const addItems = () => {
     if(text) {
-      setItems(items.concat(text))
+      items.push(text)
     }
     setText('')
   }
 
-  const handleEnter = (e) => {
-    if (e.keyCode === 13 && text) {
-        setItems(items.concat(text))
+  const handleEnter = (e: KeyboardEvent) => {
+    if (e.key === 'Enter' && text) {
+        items.push(text)
         setText('')
     }
 }
 
   const handleRandomize = () => {
     setRandom(items[Math.floor(Math.random() * items.length)])
+  }
+
+  const handleReset = () => {
+    setItems([])
+    setText('')
   }
 
   useEffect(() => {
@@ -60,7 +60,7 @@ const IndexPage = () => {
         <div className="flex flex-col w-3/4 max-w-sm justify-start items-center">
           <h1 className="text-xl mb-10 relative">Randomizer! <div className="inline-block w-auto animate-bounce">👇</div></h1>
           <div className="flex justify-center flex-wrap mb-2">
-          {items.map((item, idx) => {
+          {items.map((item: {}, idx: number) => {
             return (
             <div key={idx} className={`${borderColorsArray[idx]} border-2 rounded-lg px-4 py-2 mb-2 mr-2`}>{item}</div>
           )})}
@@ -70,8 +70,11 @@ const IndexPage = () => {
             <button className="my-8 mr-4 px-8 py-3 bg-blue-100 rounded-lg hover:bg-blue-300 hover:text-white transition-colors	duration-300" onClick={addItems}>
                 Add
             </button>
-            <button className="my-8 px-8 py-3 bg-red-100 rounded-lg hover:bg-red-300 hover:text-white transition-colors	duration-300" onClick={handleRandomize}>
+            <button className="my-8 mr-4 px-8 py-3 bg-green-100 rounded-lg hover:bg-red-300 hover:text-white transition-colors	duration-300" onClick={handleRandomize}>
                 Randomize
+            </button>
+            <button className="my-8 px-8 py-3 bg-red-100 rounded-lg hover:bg-red-300 hover:text-white transition-colors	duration-300" onClick={handleReset}>
+                Reset
             </button>
           </div>
           {random && (
